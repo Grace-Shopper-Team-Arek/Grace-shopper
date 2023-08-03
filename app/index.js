f4d317a3110afac20e5d2c5daee256e6ae30aee0
Arek Halpern <arekhalpern@areks-mbp.lan>
2023-08-02 20:40:41 -0400

    products/users api file added

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import auth from './auth';
-import cart from './cart';
+import auth from './reducers/auth';
+import cart from './reducers/cart';
 
 const reducer = combineReducers({
   auth,
@@ -13,5 +13,5 @@ const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
 
-export * from './auth';
-export * from './cart';
+export * from './reducers/auth';
+export * from './reducers/cart';
