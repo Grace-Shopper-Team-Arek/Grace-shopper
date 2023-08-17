526c87e5c4100050595202b4eff871bf335f5acb
Arek Halpern <arekhalpern@areks-mbp.lan>
2023-08-17 10:24:17 -0400

    merge

--- app/components/ReviewProduct.js
+++ app/components/ReviewProduct.js
@@ -2,12 +2,6 @@ import React from "react";
 import { connect } from 'react-redux';
 import { addNewReview, updateReview } from "../reducers/reviews";
 
-function findRating(anArray){
-    for(let i = 0; i < anArray.length -1; i++){
-        if(anArray[i].checked) return anArray[i].value;
-    }
-}
-
 function ReviewProduct(props){
     //find if the logged-in user has already reviewed product
     const existingReview = Array.from(props.reviews).filter(x => x.userId === props.userId)
@@ -26,17 +20,15 @@ function ReviewProduct(props){
             username: props.username,
         };
     
-        dataPackage.reviewText = event.target[5].value;
-        dataPackage.reviewScore = findRating(event.target);
+        dataPackage.reviewText = event.target[0].value;
+
+        //Just set the rating to 1 if the user hasn't clicked any stars yet, I do not have the werewithal for something more elegant
+        dataPackage.reviewScore = rating ? rating : 1;
+
         if(alreadyReviewed) dataPackage.reviewId = existingReview[0].id;
     
         //clear the form
-        event.target[0].checked = false;
-        event.target[1].checked = false;
-        event.target[2].checked = false;
-        event.target[3].checked = false;
-        event.target[4].checked = false;
-        event.target[5].value = "";
+        event.target[0].value = "";
     
         //send the data to be updated
         alreadyReviewed ? props.updateReview(dataPackage) : props.addNewReview(dataPackage);
@@ -75,13 +67,7 @@ function ReviewProduct(props){
     return <div>
         <h3>{alreadyReviewed ? "Update your review of this product?": "Submit a Review for this product:"}</h3>
         <form onSubmit={handleSubmit}>
-            <div>
-                <input type="radio" id="star1" name="rating" value="1" />
-                <input type="radio" id="star2" name="rating" value="2" />
-                <input type="radio" id="star3" name="rating" value="3" />
-                <input type="radio" id="star4" name="rating" value="4" />
-                <input type="radio" id="star5" name="rating" value="5" />
-            </div>
+            <Stars />
             <div>
                 <textarea type="review" placeholder="Enter your review here!" style={{width: 500, height: 250}} />
             </div>
